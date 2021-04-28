e79e5ee8b19e426f85ae4616555b622b42dd3754
NadiaTretiakova <nady.tretiakova@gmail.com>
2021-04-28 17:44:26 +0100

    added homework

---
+++ Ex3.1*_Script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /Users/nadatretakova/Desktop/Group-18/Linux_terminal # Go into the folder
+mkdir Folder1 Folder2 Folder3 # Create 3 folders
+cd Folder1/ # Go into any folder
+touch File1.txt File2.txt File3.txt File4.json File5.json # Create 5 files (3 .txt, 2 .json)
+mkdir Folder1.1 Folder1.2 Folder1.3 # Create 3 folders
+ls -la # List of the folder's content
+mv File1.txt File2.txt Folder1.1/ # Move any 2 files you created to any other folder
